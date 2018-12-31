228e93bd1d5b5314b15c77d1417f00c62b7536c5
EthanVV <ethan.vanvleck@gmail.com>
2018-12-31 13:55:22 -0600

    finish first pass fizzbuzz

--- app/containers/Challenges/FizzBuzz/index.js
+++ app/containers/Challenges/FizzBuzz/index.js
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
   width: 100%;
   height: 100%;
+  text-align: center;
 `;
 
 const Container = styled.div`
@@ -12,12 +13,13 @@ const Container = styled.div`
 `;
 
 const Title = styled.div`
-  justify-self: left;
   font-size: 2.5rem;
-  marign-left: 10rem;
 `;
 
 const Tile = styled.div`
+  display: flexbox;
+  justify-content: center;
+  align-items: center;
   width: 100%;
   height: 100%;
   background: darkgrey;
@@ -32,6 +34,22 @@ const Tile = styled.div`
   }
 `;
 
+const SlideWrapper = styled.div`
+  width: 40rem;
+  height: 2rem;
+  font-size: 1.5rem;
+  text-align: center;
+  justify-content: center;
+  justify-self: 'center';
+  .slider {
+    width: 100%;
+    height: 25px;
+    background: purple;
+    outline: none;
+    opacity: 0.7;
+  }
+`;
+
 Table.propTypes = {
   xDim: PropTypes.number.isRequired,
   yDim: PropTypes.number.isRequired,
@@ -40,11 +58,13 @@ Table.propTypes = {
 function Table(props) {
   const gridStyle = {
     display: 'grid',
-    gridTemplateColumns: `repeat(${props.xDim}, 5rem)`,
-    gridTemplateRows: `repeat(${props.yDim}, 5rem)`,
+    gridTemplateColumns: `repeat(${props.xDim}, 2rem)`,
+    gridTemplateRows: `repeat(${props.yDim}, 2rem)`,
     justifyContent: 'center',
     gap: '0.5rem',
     justifySelf: 'center',
+    textAlign: 'center',
+    paddingTop: '2rem',
   };
 
   const tiles = [];
@@ -89,10 +109,45 @@ export default class FizzBuzz extends React.PureComponent {
     };
   }
 
+  updateY(val) {
+    console.log(val);
+    this.setState({ yDim: val });
+  }
+
+  updateX(val) {
+    console.log(val);
+    this.setState({ xDim: val });
+  }
+
   render() {
     return (
       <Wrapper>
         <Title>Fizz Buzz</Title>
+        <SlideWrapper>
+          <input
+            id="X"
+            type="range"
+            min="1"
+            max="50"
+            step="1"
+            value={this.state.xDim}
+            className="slider"
+            onChange={e => this.updateX(e.target.value)}
+          />
+        </SlideWrapper>
+        <SlideWrapper>
+          <label htmlFor="y-slider">Y-Slider</label>
+          <input
+            id="Y"
+            type="range"
+            min="1"
+            max="50"
+            step="1"
+            value={this.state.yDim}
+            className="slider"
+            onChange={e => this.updateY(e.target.value)}
+          />
+        </SlideWrapper>
         <Table xDim={this.state.xDim} yDim={this.state.yDim} />
       </Wrapper>
     );
